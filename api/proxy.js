d190e566f89af1514b33301694b169f77130f59b
devrx5 <71114231+devrx5@users.noreply.github.com>
2023-07-25 09:16:10 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://scgf.ru";//your website url
+  let target = "https://dns.adguard.com/dns-query";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
