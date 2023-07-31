12f3b05d93170c05ba946d3d2cb1dd424db548fb
devrx5 <71114231+devrx5@users.noreply.github.com>
2023-07-31 06:41:56 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://2ip.ru";//your website url
+  let target = "https://www.croxyproxy.com/";
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
