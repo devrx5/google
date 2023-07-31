5ed3067ffdffc13b20c3bea5c0caa0c2538b79ae
devrx5 <71114231+devrx5@users.noreply.github.com>
2023-07-31 06:45:49 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "http://dontfilter.us/";
+  let target = " https://proxysite.cloud/";
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
